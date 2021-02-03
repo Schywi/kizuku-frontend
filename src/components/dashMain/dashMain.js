6bf1747a847902adffc51fa479a9efbc5b01bbb5
Schwyi <lucasmirandoliveira@gmail.com>
2021-02-02 23:00:30 -0300

    new design kizuku added

--- src/components/dashMain/dashMain.js
+++ src/components/dashMain/dashMain.js
@@ -11,6 +11,7 @@ const dashMain = ({ onInputChange, onButtonSubmit, imageUrl, box , entries }) =>
         <main role="main" className="dashMain">
             <article className="featured-article  dashArticle">
                     <h1 className="dashH1"> Profile</h1>
+                    <h2 className="dashH2">This Magic Brain will detect faces in your pictures. Git it a try.</h2>
                     <ImageLinkForm  onInputChange={onInputChange} onButtonSubmit={onButtonSubmit}  />
 
                     <FaceRecognition  imageUrl={imageUrl} box={box} entries={entries} />
